4cc9289df4f57797e22dd62a4cdd379fc438314e
= <c0baktr@gmail.com>
2025-12-15 22:13:12 -0700

    being good

--- bot.py
+++ bot.py
@@ -1,3 +1,10 @@
+"""
+Copyright © Krypton 2019-Present - https://github.com/kkrypt0nn (https://krypton.ninja)
+Description:
+🐍 A simple template to start to code your own and personalized Discord bot in Python
+
+Version: 6.4.0
+"""
 import json
 import logging
 import os
--- cogs/fun.py
+++ cogs/fun.py
@@ -5,6 +5,13 @@ Description:
 
 Version: 6.4.0
 """
+"""
+Copyright © Krypton 2019-Present - https://github.com/kkrypt0nn (https://krypton.ninja)
+Description:
+🐍 A simple template to start to code your own and personalized Discord bot in Python
+
+Version: 6.4.0
+"""
 
 import random
 
